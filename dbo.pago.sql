90775da99efd85f0aba55cc43f57ab4bf29174b7
Skillhh <37087716+Skillhh@users.noreply.github.com>
2018-03-10 19:31:10 -0300

    Variaciones del Programa
    Perfeccionamiento en la página de Inicio y creacion de bases de datos.

---
+++ dbo.pago.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[pago]
+(
+	[Codigo_viaje] INT NOT NULL PRIMARY KEY,
+	Codigo_id varchar(25) NOT NULL,
+	Id_motero varchar(25) NOT NULL,
+	Met_Pago varchar(25) NOT NULL,
+	Estado_pago varchar(25) NOT NULL,
+	importe varchar(25) NOT NULL,
+)
+GO
