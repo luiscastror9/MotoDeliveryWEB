dd5287df6de4a5627fa826aac6f7a21aa4185cd2
Leandro <leandrogdepaola@gmail.com>
2018-03-26 18:36:04 -0300

    act
    act

--- WebApplication8/Controllers/HomeController.cs
+++ WebApplication8/Controllers/HomeController.cs
@@ -8,6 +8,19 @@ namespace WebApplication8.Controllers
 {
     public class HomeController : Controller
     {
+        public ActionResult Demo1()
+        {
+            return View();
+        }
+        /*
+        public ActionResult Demo2()
+        {
+            var source, destination;
+            var directionsDisplay;
+            var directionsService = new google.maps.DirectionsService();
+            return View();
+        }*/
+
         public ActionResult Index()
         {
             
