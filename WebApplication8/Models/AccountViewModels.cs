b88a57ea4ec6333ecc685caf4ec1e30e3487bdb0
Skillhh <37087716+Skillhh@users.noreply.github.com>
2018-04-09 16:35:17 -0500

    GiovannyO

--- WebApplication8/Models/AccountViewModels.cs
+++ WebApplication8/Models/AccountViewModels.cs
@@ -64,50 +64,7 @@ namespace WebApplication8.Models
 
     public class RegisterViewModel
     {
-        [Required]
-        [Display(Name = "Nombre")]
-        public string nombre { get; set; }
-
-        [Required]
-        [Display(Name = "Apellido")]
-        public string apellido { get; set; }
-
-        [Required]
-        [Display(Name = "País")]
-        public string pais { get; set; }
-
-        [Required]
-        [Display(Name = "Tipo de Documento")]
-        public tipo doc_tipo { get; set; }
-
-        [Required]
-        [Display(Name = "Número")]
-        public string num_doc { get; set; }
-
-        [Required]
-        [Display(Name = "Fecha de Nacimiento")]
-        public string f_nac { get; set; }
-
-        [Required]
-        [Display(Name = "Teléfono")]
-        public string tel { get; set; }
-
-        [Required]
-        [Display(Name = "Calle")]
-        public string calle { get; set; }
-
-        [Required]
-        [Display(Name = "Altura")]
-        public string altura { get; set; }
-
-        [Display(Name = "Piso")]
-        public string piso { get; set; }
-
-        [Display(Name = "Departamento")]
-        public string depto { get; set; }
-
-        [Display(Name = "Código Postal")]
-        public string codpost { get; set; }
+       
 
         [Required]
         [EmailAddress]
@@ -125,11 +82,7 @@ namespace WebApplication8.Models
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
-    public enum tipo
-    {
-        DNI,
-        Pasaporte
-    }
+   
     public class ResetPasswordViewModel
     {
         [Required]
