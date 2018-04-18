d40ec2b040fed37de9e792fa69b830c520df966e
Skillhh <37087716+Skillhh@users.noreply.github.com>
2018-04-18 02:02:10 -0500

    Giovanny Ortegon
    Reversar modificaciones

---
+++ WebApplication8/App_Start/SendGridMessage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.ServiceModel.Channels;
+using System.Web;
+
+namespace IdentitySample.Models
+{
+    internal class SendGridMessage
+    {
+        public SendGridMessage()
+        {
+        }
+
+        public MailAddress From { get; internal set; }
+        public string Html { get; internal set; }
+        public string Subject { get; internal set; }
+        public string Text { get; internal set; }
+
+        internal void AddTo(string destination)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
