6faf705fa419b85c60b85bf2856d7932c50560ce
Skillhh <37087716+Skillhh@users.noreply.github.com>
2018-05-03 07:12:30 -0500

    Enviar correo

--- WebApplication8/App_Start/IdentityConfig.cs
+++ WebApplication8/App_Start/IdentityConfig.cs
@@ -18,8 +18,35 @@ namespace WebApplication8
     {
         public Task SendAsync(IdentityMessage message)
         {
-            // Plug in your email service here to send an email.
-            return Task.FromResult(0);
+            // Credentials:
+            var credentialUserName = "motodeliverycomit@outlook.com";
+            var sentFrom = "motodeliverycomit@outlook.com";
+            var pwd = "MotoDelivery2018";
+
+            // Configure the client:
+            System.Net.Mail.SmtpClient client =
+                new System.Net.Mail.SmtpClient("smtp-mail.outlook.com");
+
+            client.Port = 587;
+            client.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+
+            // Creatte the credentials:
+            System.Net.NetworkCredential credentials =
+                new System.Net.NetworkCredential(credentialUserName, pwd);
+
+            client.EnableSsl = true;
+            client.Credentials = credentials;
+
+            // Create the message:
+            var mail =
+                new System.Net.Mail.MailMessage(sentFrom, message.Destination);
+
+            mail.Subject = message.Subject;
+            mail.Body = message.Body;
+
+            // Send:
+            return client.SendMailAsync(mail);
         }
     }
 
